a5ad88f05e3b711f34231b38eee39efbd957a77a
kevin <kevinbarudde@hotmail.com>
2021-03-30 14:27:13 +0200

    fixed exampel code

--- exempel_databas.py
+++ exempel_databas.py
@@ -1,225 +1,193 @@
-from flask_restful import Resource, Api, reqparse
-import os
-from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import (
-        JWTManager, jwt_required, get_jwt_identity,
-        create_access_token, get_raw_jwt)
-from datetime import timedelta
+from server import app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_
+from flask import json
+from datetime import datetime
+from sqlalchemy.orm.exc import NoResultFound
+from flask_jwt_extended import decode_token
 
 
-#FLASK_APP=server.py flask run
+db = SQLAlchemy(app)
 
-app = Flask(__name__)
-app.debug = True
 
+#----------------------------DATA---------------------------------------------
+readBy = db.Table('readBy',
+    db.Column('message_id', db.Integer, db.ForeignKey('Message.id'),primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('User.id'), primary_key=True)
+    )
 
-if 'NAMESPACE' in os.environ and os.environ['NAMESPACE'] == 'heroku':
-    db_uri = os.environ['DATABASE_URL']
-    debug_flag = False
-else: # when running locally with sqlite
-    db_path = os.path.join(os.path.dirname(__file__), 'app.db')
-    db_uri = 'sqlite:///{}'.format(db_path)
-    debug_flag = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
-app.config['JWT_SECRET_KEY'] = os.urandom(20).hex()
-app.config['JWT_BLACKLIST_ENABLED'] = True
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=7)
+class Message(db.Model):
+    __tablename__ = 'Message'
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(70), nullable=False, unique=False)
+    readBy = db.relationship('User', secondary=readBy, lazy='subquery',
+        backref=db.backref('messages', lazy=True))
 
+    def __init__(self, msg):
+        self.message = msg
 
-bcrypt = Bcrypt(app)
-import data_handler
-jwt = JWTManager(app)
-api = Api(app)
 
+    def get_message(self):
+        return self.message
 
-# Parser.
-parser = reqparse.RequestParser()
-parser.add_argument('message')
-parser.add_argument('username')
-parser.add_argument('password')
 
+    def get_msgID(self):
+        return self.id
 
-@jwt.token_in_blacklist_loader
-def check_if_token_in_blacklist(decrypted_token):
-    jti = decrypted_token['jti']
-    return jti in data_handler.get_blacklist_jtis()
+    def get_readBy(self):
+        return [x.get_username() for x in self.readBy]
 
 
-#----------------------------SERVER-------------------------------------------
-class Messages(Resource): #/messages
+    def get_all(self):
+        return {'id':self.id,
+                'message':self.message,
+                'readBy':self.get_readBy()}
 
+    def __repr__(self):
+        return json.dumps({'id':self.id,
+                'message':self.message,
+                'readBy':self.get_readBy()})
 
-    def get(self):
-        """GET-call for /messages, returns message and HTTP 200."""
-        all_msg = data_handler.get_all_messages()
-        return all_msg , 200
+class User(db.Model):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(), nullable=False, unique=False)
 
-    @jwt_required
-    def post(self):
-        """POST-call for ../messages, adds message to our message list
-        and gives it a unique ID if valid data given. Returns message-ID and
-        HTTP 200.
-        """
-        message = parser.parse_args()['message']
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
 
-        print(message)
+    def get_id(self):
+        return self.id
 
-        if not isinstance(message,str) or len(message) > 140:
-            return 'No valid data given' , 400
+    def get_username(self):
+        return self.username
 
-        id = data_handler.store_message(message)
-        return {'id': id}, 200
+    def __repr__(self):
+        return json.dumps({'username':self.username})
 
 
-class MessageID(Resource): #/messages/<messageID>
+class Token(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    iat = db.Column(db.String(10),nullable=False)
+    jti = db.Column(db.String(36), nullable=False)
+    expires = db.Column(db.DateTime, nullable=False)
+    user_identity = db.Column(db.String(50), nullable=False)
+    token_type = db.Column(db.String(10), nullable=False)
 
-    def get(self,messageID):
-        """GET-call for /messages/<messageID>, returns the messages with
-        desired message-ID to the user."""
 
-        message = data_handler.get_messageID(messageID)
-        if message is None:
-            return "Invalid message id.", 404
-        else:
-            return message.get_all(), 200
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'iat' : self.iat,
+            'jti': self.jti,
+            'expires': self.expires,
+            'user_identity': self.user_identity,
+            'token_type': self.token_type
+            }
 
-    @jwt_required
-    def delete(self,messageID):
-        """ Deletes all message of given message-ID,
-        returns HTTP 200 if successful, otherwise returns HTTP 404 """
+#----------------------------DATA---------------------------------------------
 
-        msg = data_handler.get_messageID(messageID)
-        if msg is None:
-            return "Invalid message ID.", 404
-        else:
-            data_handler.delete_message(msg)
-            return 'Message removed.',200
+#--------------------------Sepret Functions-----------------------------------
+def store_message(msg):
+    new_message = Message(msg)
+    db.session.add(new_message)
+    db.session.commit()
 
+    return new_message.id
 
-class Unread(Resource): #/messages/unread/<username>
 
+def delete_message(msg):
+    db.session.delete(msg)
+    db.session.commit()
 
-    @jwt_required
-    def get(self,username):
-        """ GET-call for messages/unread/<username>, returns list with desired
-        user-ID. """
-        result = []
-        messages = data_handler.get_unread(username)
-        for message in messages:
-            result.append({'id':message.message_id,
-                        'message':message.message,
-                        'readBy':message.get_readBy()})
 
-        return result
+def create_user(username,password):
+    new_user = User(username,password)
+    db.session.add(new_user)
+    db.session.commit()
 
+def add_read(user,msg):
+    msg.readBy.append(user)
+    db.session.commit()
 
-class Flag(Resource): # /messages/<messageID>/flag/<username>
+def init():
+    db.drop_all()
+    db.create_all()
 
 
-    @jwt_required
-    def post(self, messageID, username):
-        """ Flags a given message as read by given user."""
+def add_token(token):
+    new_token = Token(token)
+    db.session.add(new_token)
+    db.session.commit()
 
-        msg = data_handler.get_messageID(messageID)
-        user = data_handler.get_user(username)
+def get_all_messages():
+    return [x.get_all() for x in Message.query.all()]
 
-        if msg is None:
-            return 'Invalid message ID.', 404
-        if user is None:
-            return 'Invaild username', 404
+def get_all_messagesID():
+    return [x.get_msgID() for x in Message.query.all()]
 
-        data_handler.add_read(user,msg)
+def get_messageID(messageID):
+    return Message.query.filter_by(id=messageID).first()
 
-        return 'Message read' , 200
 
+def get_user(username):
+    return User.query.filter_by(username=username).first()
 
-class Create(Resource): # /user
 
+        #msg = data_handler.get_all_messages()
+        #temp_list = []
 
-    def post(self):
-        args = parser.parse_args()
-        username = args['username']
+        #for el in msg:
 
+        #    if not userID in el['readBy']:
+        #        temp_list += [el]
 
-        if data_handler.get_user(username) != None:
-            return 'username in use',401
-        if username == None:
-            return 'no username given', 405
-        if args['password'] == None:
-            return 'no password given' , 405
+        #return temp_list, 200
+def get_unread(username):
+    message = db.session.query(Message).\
+        outerjoin(Message.readBy).filter(or_(User.username.is_(None), User.username != username)).distinct(User.username).all()
+    return message
 
-        password = bcrypt.generate_password_hash(args['password']).decode('utf-8')
+def get_blacklist_jtis():
+    return [token.jti for token in Token.query.all()]
 
-        data_handler.create_user(username,password)
-        return 'user created', 200
 
+def _epoch_utc_to_datetime(epoch_utc):
+    return datetime.fromtimestamp(epoch_utc)
 
-class Login(Resource): # /user/login
-    def post(self):
-        data_handler.prune_database()
 
-        args = parser.parse_args()
-        user = data_handler.get_user(args['username'])
+def add_token_to_database(token, identity_claim):
 
-        if user == None:
-            return "{'Error':'No such user'}",401
-        if args['password'] == None:
-            return 'no password given' , 405
+    iat = token['iat']
+    jti = token['jti']
+    token_type = token['type']
+    expires = _epoch_utc_to_datetime(token['exp'])
 
-        if bcrypt.check_password_hash(user.password,args['password']):
-            access_token = create_access_token(identity=user.username)
+    db_token = Token(
+        iat=iat,
+        jti=jti,
+        token_type=token_type,
+        user_identity=identity_claim,
+        expires=expires,
+    )
+    db.session.add(db_token)
+    db.session.commit()
 
-            return {'token':access_token},200
-        return "{'Error':'Wrong password'}", 400
 
+def prune_database():
+    """
+    Delete tokens that have expired from the database.
+    How (and if) you call this is entirely up you. You could expose it to an
+    endpoint that only administrators could call, you could run it as a cron,
+    set it up with flask cli, etc.
+    """
+    now = datetime.now()
+    expired = Token.query.filter(Token.expires < now).all()
+    for token in expired:
+        db.session.delete(token)
+    db.session.commit()
 
-class Logout(Resource): # /user/logout
-#,RevokedTokenError
-
-    @jwt_required
-    def post(self):
-        data_handler.add_token_to_database(get_raw_jwt(),get_jwt_identity())
-        return 'Logout successful',200
-
-
-class init_db(Resource): # /init_dbinit
-
-    def get(self):
-        data_handler.init()
-        return '',200
-
-
-class welcome(Resource):
-
-    def get(self):
-        return "Welcome"
-
-
-
-api.add_resource(welcome, '/')
-
-api.add_resource(Create, '/user')
-
-api.add_resource(Login, '/user/login')
-
-api.add_resource(Logout, '/user/logout')
-
-api.add_resource(Unread, '/messages/unread/<username>')
-
-api.add_resource(Flag, '/messages/<messageID>/flag/<username>')
-
-api.add_resource(MessageID, '/messages/<messageID>')
-
-api.add_resource(Messages, '/messages')
-
-api.add_resource(init_db, '/init_db')
-
-
-#----------------------------SERVER-------------------------------------------
-
-if __name__ == "__main__":
-    data_handler.init()
+#--------------------------Sepret Functions-----------------------------------
