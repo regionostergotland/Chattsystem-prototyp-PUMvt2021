09c6de297df3fb80ccb22cdb1c008d0923e51169
casper <casje896@student.liu.se>
2021-04-06 10:35:01 +0200

    Test för lucys sida om tensorflow funkar fint.

--- code/Chattbott/training.py
+++ code/Chattbott/training.py
@@ -20,7 +20,8 @@ from tensorflow.keras.optimizers import SGD
 lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open('intents.json').read())
-
+print(intents)
+"""
 words = []
 classes = []
 documents = []
@@ -75,3 +76,4 @@ model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy
 model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 model.save('chatbot_model.model')
 print("Done")
+"""
