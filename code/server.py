b60c2d89dd05dc5174c8401c1a49632159843a3e
Hannes Westander <hanwe828@student.liu.se>
2021-04-07 15:43:25 +0200

    Strukturerart om servern, fokus på client och chattar

--- code/server.py
+++ code/server.py
@@ -1,22 +1,25 @@
 from typing import Collection
+from enum import Enum 
 from flask import Flask, render_template, send_from_directory, request
 from flask_socketio import SocketIO
 import authentication
 
 # The Client class
 class Client:
-  def __init__(self, sid, name, backgroundColor, userIconSource):
+  def __init__(self, sid, name, backgroundColor, userIconSource, role):
     self.sid = sid
     self.name = name
     self.backgroundColor = backgroundColor
     self.userIconSource = userIconSource
     self.authenticated = False
+    self.role = role 
 
 # The Chat class
 class Chat:
   def __init__(self, clients = []):
     self.history = []
     self.clients = clients
+    self.active = True
 
 
 # The Message class
@@ -24,6 +27,12 @@ class Message:
   def __init__(self, sender, text):
     self.sender = sender
     self.text = text
+
+#Enum for roles
+class Roles (Enum): 
+  odefinierad = 1
+  patient = 2 
+  personal = 3
 
 # Initializes the server
 app = Flask(__name__)
@@ -55,7 +64,7 @@ def send_images(path):
 clients = []
 
 # All the chats
-chats = [Chat()]
+chats = {"huvudchatt":Chat()}
 
 
 # The event for authenticating clients
@@ -64,8 +73,10 @@ def authenticate_event(methods=['GET', 'POST']):
   client = get_client(request.sid)
   if(not client.authenticated):
     client.authenticated = authentication.authenticate(client)
-  json = {'status': 200, 'message':'Du är nu autentiserad.'} if client.authenticated else {'status': 400, 'message':'Det uppstod ett fel vid autentisering!'}
-  socketio.emit('info', json, room=request.sid)
+  if client.authenticated :
+    send_info_message(200, "Du är nu autentiserad", request.sid)
+  else : 
+    send_info_message(400, "Det uppstod ett fel vid autentisering", request.sid)
 
 # Temporary data
 names = ["Ludwig", "Sven", "Anna", "Emma", "Peter", "Kalle"]
@@ -77,40 +88,109 @@ userIconSources = ["/images/user.png", "/images/bot.png"]
 def connect_event(methods=['GET', 'POST']):
   print("\nUser connected: " + request.sid)
   currentSocketId = request.sid
-  name = names[len(clients)%len(names)]
-  backgroundColor = backgroundColors[len(clients)%len(backgroundColors)]
+  backgroundColor = "white"
   userIconSource = userIconSources[0]
-  client = Client(currentSocketId, name, backgroundColor, userIconSource)
+  name = "anonym"
+  role = Roles["odefinierad"]
+  client = Client(currentSocketId, name, backgroundColor, userIconSource, role)
   clients.append(client)
-  chats[0].clients.append(client)
-  send_chat_history(client,chats[0])
+ 
+# The event for asigning credentials
+@socketio.on('details_assignment')
+def details_assignment_event(json, methods=['GET', 'POST']):
+  client = get_client(request.sid)
+  client.name = json["name"]
+  client.backgroundColor = json["backgroundColor"]
+  client.userIconSource = json["userIconSource"]
+  client.role = json["role"]
+  send_info_message(200, "Användarinformation satt", request.sid)
+
 
 # The event for when a message is recieved from a client
 @socketio.on('message')
 def message_event(json, methods=['GET', 'POST']):
-  print("\nMessage: " + json['message'])
+  print("\nMessage: " + json['message'] + "(" + json['chatName'] + ")")
   sender = get_client(request.sid)
-  message = Message(sender, json['message'])
-  chats[0].history.append(message)
-  broadcast_message(message)
+  chatName = json["chatName"]
+  if chatName in chats :
+    chat = chats[chatName]
+    if chat.active :
+      message = Message(sender, json['message'])
+      chat.history.append(message)
+      broadcast_message(message, chatName)
+    else :
+      send_info_message(200, "Chatten är avslutad", request.sid)
+  else :
+    send_info_message(404, "Chatten finns inte", request.sid)
+  
+
+# The event for creating a new chat
+@socketio.on('chat_create')
+def chat_create_event(json, methods=['GET', 'POST']):
+  client = get_client(request.sid)
+  chatName = json["chatName"]
+  if chatName in chats : 
+    send_info_message(400, "Chattnamnet är redan taget", request.sid)
+  else : 
+    chats[chatName] = Chat([client])
+    send_info_message(200, "Chatten är skapad", request.sid)
+
+# The event for removing a chat
+@socketio.on('chat_delete')
+def chat_delete_event(json, methods=['GET', 'POST']):
+  client = get_client(request.sid)
+  chatName = json["chatName"]
+  if chatName in chats : 
+    del chats[chatName]
+    send_info_message(200, "Chatten är borttagen", request.sid)
+  else : 
+    send_info_message(404, "Chatten finns inte", request.sid)
+
+# The event for ending a chat
+@socketio.on('chat_end')
+def chat_end_event(json, methods=['GET', 'POST']):
+  client = get_client(request.sid)
+  chatName = json["chatName"]
+  if chatName in chats : 
+    chats[chatName].active = False
+    send_info_message(200, "Chatten är avslutad", request.sid)
+  else : 
+    send_info_message(404, "Chatten finns inte", request.sid)
+
+# The event for creating a new chat
+@socketio.on('chat_join')
+def chat_join_event(json, methods=['GET', 'POST']):
+  client = get_client(request.sid)
+  chatName = json["chatName"]
+  if chatName in chats : 
+    chats[chatName].clients.append(client)
+    send_info_message(200, "Klienten har anslutit till chatten", request.sid)
+  else : 
+    send_info_message(404, "Chatten finns inte", request.sid)
+
+# Sends the given info message
+def send_info_message(statusCode, message, sid):
+  json = {'status': statusCode, 'message':message}
+  socketio.emit('info', json, room=sid)
 
 # Sends the whole chat history of the given chat to a client
-def send_chat_history(reciever,chat):
+def send_chat_history(reciever,chatName):
+  chat = chats[chatName]
   for message in chat.history:
-    send_message(message, reciever) 
+    send_message(message, reciever,chatName) 
 
 # Broadcasts the message to all clients
-def broadcast_message(message, ignoreSender = True):
+def broadcast_message(message, chatName, ignoreSender = True):
   sender = message.sender
-  json = {'sender': sender.name, 'icon-source': sender.userIconSource, 'background': sender.backgroundColor, 'message':message.text}
-  for client in clients:
+  json = {'sender': sender.name, 'icon-source': sender.userIconSource, 'background': sender.backgroundColor, 'message':message.text, 'chatName':chatName}
+  for client in chats[chatName].clients:
     if(not ignoreSender or not client.sid == sender.sid):
       socketio.emit('message', json, room=client.sid)  
 
 # Sends the message to a client
-def send_message(message, reciever):
+def send_message(message, reciever, chatName):
   sender = message.sender
-  json = {'sender': sender.name, 'icon-source': sender.userIconSource, 'background': sender.backgroundColor, 'message':message.text}
+  json = {'sender': sender.name, 'icon-source': sender.userIconSource, 'background': sender.backgroundColor, 'message':message.text, 'chatName':chatName}
   socketio.emit('message', json, room=reciever.sid)
 
 # Returns the client with a session id
