be3c4667d638b8cb6b5b6cf89392d0157c167c7c
Filip Karlberg <filka201@student.liu.se>
2021-05-03 09:45:11 +0200

    autentisering color picker

--- code/scripts/compiled-javascript/chattpersonal.js
+++ code/scripts/compiled-javascript/chattpersonal.js
@@ -3,13 +3,17 @@ var socket;
 var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("authButton");
+// User name choice
 var nameInput = document.getElementById("nameInput");
+// User color choice
+var authColor = document.getElementById("authColor");
+
 btn.addEventListener("click", (e) => {
     if (nameInput.value != "") {
         console.log(nameInput.value);
         modal.style.display = "none";
         socket.emit('details_assignment', {
-            name: nameInput.value, backgroundColor: "red", userIconSource: "/images/user.png", role: "personal"
+            name: nameInput.value, backgroundColor: authColor.value, userIconSource: "/images/user.png", role: "personal"
         });
         socket.emit('authenticate');
     }
